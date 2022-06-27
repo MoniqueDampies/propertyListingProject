c92b763c125119fa71fff21c7e52c302ffaeaec6
MoniqueDampies <moniquedampies1861@gmail.com>
2022-06-27 16:13:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,192 @@
+document.getElementById("card1").innerHTML = `
+<img src="./assets/008-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link text-end"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
+document.getElementById("card2").innerHTML = `
+<img src="./assets/012-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
+document.getElementById("card3").innerHTML = `
+<img src="./assets/018-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
+document.getElementById("card4").innerHTML = `
+<img src="./assets/020-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
+document.getElementById("card5").innerHTML = `
+<img src="./assets/026-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
+document.getElementById("card6").innerHTML = `
+<img src="./assets/030-592x444.jpg" width="100%">
+<h5>$11,000/mo</h5>
+<div class="text-start px-3 pt-2">
+<h5>New Apartment Nice View</h5>
+<p>6701 South Dixie Highway, Miami, FL, USA</p>
+
+
+<p>
+<i class="fa-solid fa-bed"></i> : 3
+
+<i class="fa-solid fa-shower"></i> : 1
+
+<i class="fa-solid fa-car"></i> : 1
+
+e08d : 1200 Sq Ft
+</p>
+
+<p><b>Apartment</b></p>
+</div>
+<hr>
+<div class="container">
+<div class="row">
+<div class="col-6">
+<p class="text-start"><i class="fa-solid fa-user"></i>Shane Stevens</p>
+</div>
+<div class="col-6">
+<p><i class="fa-solid fa-link"></i>2 years ago</p>
+</div>
+</div>
+</div>
+`;
